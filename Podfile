69172413196ca753e865edd70cfdcb0e5cb9aea5
donnasamuel88 <donnasamuel@gmail.com>
2018-01-19 14:38:36 +0800

    Completely setting up Firebase integration

---
+++ Podfile
@@ -0,0 +1,15 @@
+project 'socialNetwork.xcodeproj'
+
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'socialNetwork' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
+  pod 'Firebase/Storage'
+  # Pods for socialNetwork
+
+end
---
+++ socialNetwork/Controller/SignInVC.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  socialNetwork
+//
+//  Created by Donna Samuel on 19/1/18.
+//  Copyright © 2018 donnasamuel. All rights reserved.
+//
+
+import UIKit
+
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
