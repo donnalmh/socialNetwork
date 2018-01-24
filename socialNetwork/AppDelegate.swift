74455f9d23d3ca4ea75cb6e659a532940c00b6aa
donnasamuel88 <donnasamuel@gmail.com>
2018-01-24 09:52:17 +0800

    End of current branch. Committing into a new branch

--- socialNetwork/AppDelegate.swift
+++ socialNetwork/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Firebase
 import FBSDKCoreKit
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,8 +20,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        return FBSDKApplicationDelegate.sharedInstance().application(_:app, open: url, options: options)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- socialNetwork/Controller/SignInVC.swift
+++ socialNetwork/Controller/SignInVC.swift
@@ -7,15 +7,48 @@
 //
 
 import UIKit
-
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var fbButton: RoundBtn!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+
+    @IBAction func fbButtonPressed(_ sender: Any) {
+        let fbLogin = FBSDKLoginManager()
+        print("logging in facebook")
+        fbLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Donna: Unable to authenticate with Facebook: \(error)")
+            }else if result?.isCancelled == true{
+                print("Donna: User cancelled Facebook Authentication")
+            }else{
+                print("Donna: Authenticated successfully on Facebook")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.fireBaseAuth(credential)
+            }
+        }
+        
+    }
+    
+    func fireBaseAuth(_ credential: AuthCredential){
+        Auth.auth().signIn(with: credential) { (user, error) in
+            if error != nil {
+                print("Donna: Unable to authenticate  with FireBase:\(error.debugDescription)")
+            } else  {
+                print("Donna: Authenticated successfully on Firebase")
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
